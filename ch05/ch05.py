d946bdd0860a818711f15bde29cd5f6ba29246c2
Aoki-Dai <108746684+Aoki-Dai@users.noreply.github.com>
2025-08-07 13:56:03 +0900

    全分散と寄与分散のコードにコメントを追加

--- ch05/ch05.py
+++ ch05/ch05.py
@@ -108,20 +108,22 @@ df_wine.head()
 
 
 
-
+# 2列目のデータをXに、1列目のデータをyに格納
 X, y = df_wine.iloc[:, 1:].values, df_wine.iloc[:, 0].values
-
-X_train, X_test, y_train, y_test = \
-    train_test_split(X, y, test_size=0.3, 
-                     stratify=y,
-                     random_state=0)
+# 訓練データ70%とテストデータ30%に分割
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.3, 
+    stratify=y,
+    random_state=0
+)
 
 
 # データの標準化。
 
 
 
-
+# 特徴量の標準化
+# 平均0、分散1に変換
 sc = StandardScaler()
 X_train_std = sc.fit_transform(X_train)
 X_test_std = sc.transform(X_test)
@@ -175,7 +177,9 @@ X_test_std = sc.transform(X_test)
 
 
 
+# 共分散行列を作成
 cov_mat = np.cov(X_train_std.T)
+# 共分散行列の固有値と固有ベクトルを計算
 eigen_vals, eigen_vecs = np.linalg.eig(cov_mat)
 
 print('\nEigenvalues \n', eigen_vals)
@@ -192,19 +196,25 @@ print('\nEigenvalues \n', eigen_vals)
 
 
 
+# 固有値を合計
 tot = sum(eigen_vals)
+# 分散説明率を計算
 var_exp = [(i / tot) for i in sorted(eigen_vals, reverse=True)]
+# 分散説明率の累積和を計算
 cum_var_exp = np.cumsum(var_exp)
 
 
 
 
 
-
+# 分散説明率の棒グラフを作成
 plt.bar(range(1, 14), var_exp, align='center',
         label='Individual explained variance')
+
+# 分散説明率の累積和の階段グラフを作成
 plt.step(range(1, 14), cum_var_exp, where='mid',
          label='Cumulative explained variance')
+
 plt.ylabel('Explained variance ratio')
 plt.xlabel('Principal component index')
 plt.legend(loc='best')
